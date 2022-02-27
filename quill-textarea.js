ad4485389421034a6409e2c4130d12ff23ab8a43
Dinesh Kumar <57162970+kumar-dinesh@users.noreply.github.com>
2022-02-27 23:18:18 +0530

    Variables renamed

--- quill-textarea.js
+++ quill-textarea.js
@@ -1,42 +1,42 @@
-function quilljs_textarea(elem = null, options = null) {
-    if(elem) {
-        var editorElems = Array.prototype.slice.call(document.querySelectorAll(elem));
+function quilljs_textarea(selector = null, options = null) {
+    if(selector) {
+        var elements = Array.prototype.slice.call(document.querySelectorAll(selector));
     } else {
-        var editorElems = Array.prototype.slice.call(document.querySelectorAll('[data-quilljs]'));
+        var elements = Array.prototype.slice.call(document.querySelectorAll('[data-quilljs]'));
     }
-    editorElems.forEach(function(el) {
-    if(elem && el.hasAttribute("data-quilljs")) {
-        return;
-    }
-    var elemType = el.type;
-    if(elemType == 'textarea') {
-        elemValue = el.value;
-        editorDiv = document.createElement('div');
-        editorDiv.innerHTML = elemValue;
-        el.parentNode.insertBefore(editorDiv, el.nextSibling);
-        el.style.display = "none";
-        var placeholder = el.placeholder;
-    } else {
-        var placeholder = null;
-        editorDiv = el;   
-    }
-    if(!options) {
-        var default_options = {
-        theme: 'snow',
-        placeholder: placeholder,
-        };
-    } else {
-        if(!options.placeholder) {
-        options.placeholder = placeholder;
+    elements.forEach(function(el) {
+        if(selector && el.hasAttribute("data-quilljs")) {
+            return;
         }
-        var default_options = options;
-    }
+        var type = el.type;
+        if(type == 'textarea') {
+            value = el.value;
+            container = document.createElement('div');
+            container.innerHTML = value;
+            el.parentNode.insertBefore(container, el.nextSibling);
+            el.style.display = "none";
+            var placeholder = el.placeholder;
+        } else {
+            var placeholder = null;
+            container = el;
+        }
+        if(!options) {
+            var defaults = {
+                theme: 'snow',
+                placeholder: placeholder,
+            };
+        } else {
+            if(!options.placeholder) {
+                options.placeholder = placeholder;
+            }
+            var defaults = options;
+        }
 
-    var editor = new Quill(editorDiv, default_options);
-    editor.on('text-change', function(delta, oldDelta, source) {
-        var editor_value = editor.root.innerHTML;
-        el.value = editor_value;
-    });
+        var editor = new Quill(container, defaults);
+        editor.on('text-change', function() {
+            var text = editor.root.innerHTML;
+            el.value = text;
+        });
     });
 }
 (function() {
